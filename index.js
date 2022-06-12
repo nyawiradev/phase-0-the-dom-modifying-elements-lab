acf5b6a8c8819b67a0449824f73731f1c8f84f49
nyawiradev <njaginjoyce@gmail.com>
2022-06-12 18:11:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainNode = document.getElementById('main');
+mainNode.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Joy is the champion';
